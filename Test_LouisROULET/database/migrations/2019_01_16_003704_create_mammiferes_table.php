b9d5f71f75f31b73eee6be1dbcd24e4d4cdd8cd6
GitLouisR <louis-roulet@hotmail.fr>
2019-01-18 11:21:16 +0100

    Update 2019_01_16_003704_create_mammiferes_table.php
    change ondelete/onupdate from 'restrict to 'cascade'

--- Test_LouisROULET/database/migrations/2019_01_16_003704_create_mammiferes_table.php
+++ Test_LouisROULET/database/migrations/2019_01_16_003704_create_mammiferes_table.php
@@ -21,8 +21,8 @@ class CreateMammiferesTable extends Migration
 			$table->foreign('animal_id')
 				  ->references('id')
 				  ->on('animals')
-				  ->onDelete('restrict')
-				  ->onUpdate('restrict');
+				  ->onDelete('cascade')
+				  ->onUpdate('cascade');
 		
         });
     } 
